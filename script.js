ab5c050e4c5717467d6d51ed61817d63d3fc036d
ketki kumeriya <111123802+Kumeriyakajal@users.noreply.github.com>
2023-08-04 11:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //your JS code here. If required.
-let div=document.createElement("div");
-div.id='browser-info';
-document.body.append(div);
-div.innerHTML="You are using "+navigator.appName+" version "+navigator.appVersion;
+function livetime() {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+  let cudate = `${month}/${day}/${year}`;
+  document.getElementById('timer').textContent = cudate + ', ' + time;
+}
+function startTimer() {
+  livetime();
+  setInterval(livetime, 1000); 
+}
+ 
+window.addEventListener('load', startTimer);
