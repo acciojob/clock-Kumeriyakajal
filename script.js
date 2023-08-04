4c6daf703304783d6131230ceb7e65af4ff8bbe7
ketki kumeriya <111123802+Kumeriyakajal@users.noreply.github.com>
2023-08-04 11:39:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let div=document.createElement("div");
+div.id='browser-info';
+document.body.append(div);
+div.innerHTML="You are using "+navigator.appName+" version "+navigator.appVersion;
